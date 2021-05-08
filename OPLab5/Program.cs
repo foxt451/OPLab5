9e3fede22cbf7228bea146a25bf255c39f08d3cf
Anatolii <Parshykov>
2021-05-08 12:07:41 +0300

    practic

--- OPLab5/Program.cs
+++ OPLab5/Program.cs
@@ -10,7 +10,8 @@ namespace OPLab5
     {
         static void Main(string[] args)
         {
-            args = new[] {"--db=data.csv", " --lat=48,85"," --long=37,6","--size=0,5"};
+            //args = new[] {"--db=data.csv", " --lat=50"," --long=29","--size=5"};
+            args = new[]{"--db=data.csv", "--lat=50", "--long=29", "--number=5", "--type=leisure", "nearest"};
             string path = Directory.GetCurrentDirectory();
             List<EarthPoint> points = FileWork.ReadFile(path+"\\"+args[0].Split('=')[1]);
             RTree tree = new RTree();
@@ -27,11 +28,17 @@ namespace OPLab5
                     double.Parse(args[2].Split('=')[1]),
                     double.Parse(args[3].Split('=')[1]), args[4].Split('=')[1]);
             }
-            else
+            else if(args.Length == 4)
             {
                 earthPoints = searcher.Search(tree, double.Parse(args[1].Split('=')[1]),
                     double.Parse(args[2].Split('=')[1]),
                     double.Parse(args[3].Split('=')[1]));
+            }
+            else if (args.Length == 6)
+            {
+                earthPoints = searcher.SearchNearest(tree, double.Parse(args[1].Split('=')[1]),
+                    double.Parse(args[2].Split('=')[1]),
+                    int.Parse(args[3].Split('=')[1]), args[4].Split('=')[1]);
             }
 
             for (int i = 0; i < earthPoints.Count; i++)
--- OPLab5/SearchLocation.cs
+++ OPLab5/SearchLocation.cs
@@ -1,13 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
 namespace OPLab5
 {
     public class SearchLocation
-    {
-        private List<EarthPoint> pointInCircle = new List<EarthPoint>();
-        public List<EarthPoint> Search(RTree tree, double center1, double center2, double radius, string type = "unknown")
+    { public List<EarthPoint> Search(RTree tree, double center1, double center2, double radius, string type = "unknown")
         {
+            List<EarthPoint> pointInCircle = new List<EarthPoint>();
             Circle circle = new Circle(center1, center2, radius);
             Stack<RTreeNode> nodes = new Stack<RTreeNode>(); 
             nodes.Push(tree.root);
@@ -46,5 +46,32 @@ namespace OPLab5
 
             return pointInCircle;
         }
+
+        public List<EarthPoint> SearchNearest(RTree tree, double center1, double center2,int n, string type = "unknown")
+        {
+            double i = 0.1;
+            while (Search(tree, center1, center2, i, type).Count<n)
+            {
+                i += 0.1;
+            }
+
+            List<EarthPoint> nearestPoints = Search(tree, center1, center2, i, type);
+            nearestPoints.Sort((EarthPoint p1, EarthPoint p2) => Compare(center1, center2, p1, p2));
+            List<EarthPoint> result = new List<EarthPoint>();
+            for (int j = 0; j < n; j++)
+            {
+                result.Add(nearestPoints[j]);
+            }
+
+            return result;
+        }
+
+        private int Compare(double lat1, double long1, EarthPoint point1, EarthPoint point2)
+        {
+            double a = Math.Round(RecAndCircle.DistanceTwoPoints(lat1, point1.latitude, long1, point1.longitude),5);
+            double b = Math.Round(RecAndCircle.DistanceTwoPoints(lat1, point2.latitude, long1, point2.longitude), 5);
+            return a > b ? 1 : a==b?0:-1;
+        }
+        
     }
 }
